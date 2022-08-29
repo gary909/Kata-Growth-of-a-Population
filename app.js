fc3d06f3d37d6e5f32cf27aac23939396d278898
gary909 <garywhite61@hotmail.com>
2022-08-29 13:02:50 +0200

    codfe

---
+++ app.js
@@ -0,0 +1,7 @@
+function nbYear(p0, percent, aug, p) {
+    // your code
+}
+
+console.log(nbYear(1500, 5, 100, 5000)); // 15
+console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
+console.log(nbYear(1500000, 0.25, 1000, 2000000)); // 94
